b270b15eaea1d447c0fe8110b327f53716e7991e
unknown <linus.lindroth.92@gmail.com>
2025-02-25 14:13:18 +0100

    added session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
+
+  const setSession = async () => {
+    const response = await fetch('/api/set-session', { method: 'POST', credentials: 'include' })
+    const data = await response.json()
+    console.log(data)
+  }
+
+  const getSession = async () => {
+    const response = await fetch('/api/get-session', { credentials: 'include' })
+    const data = await response.json()
+    if (response.ok) {
+      setUser(data)
+    } else {
+      console.log("Error getting session data")
+    }
+  }
+
+  const clearSession = async () => {
+    const response = await fetch('/api/clear-session', { method: 'DELETE', credentials: 'include' })
+    const data = await response.json()
+    console.log(data)
+  }
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>This is a session demo</h1>
+        <button onClick={setSession}>Set Session</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>Session Data</h1>
+        <button onClick={getSession}>Get Session</button>
+        <p>{user}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button onClick={clearSession}>Clear Session</button>
+      </div>
     </>
   )
 }
+
 
 export default App
--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,53 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 var app = builder.Build();
+
+app.UseSession();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.MapPost("/api/set-session", (Delegate)SetSession);
+app.MapGet("/api/get-session", (Delegate)GetSession);
+app.MapDelete("/api/clear-session", (Delegate)ClearSession);
+
+async Task<IResult> SetSession(HttpContext context)
+{
+    if (context.Session.GetString("email") != null)
+    {
+        return Results.BadRequest("Session already set");
+    }
+    Console.WriteLine("SetSession is called..Setting session");
+    await Task.Run(() => context.Session.SetString("email", "superadmin@admin.com"));
+    return Results.Ok(context.Session.GetString("email"));
+}
+
+async Task<IResult> GetSession(HttpContext context)
+{
+    Console.WriteLine("GetSession is called..Getting session");
+    var email = await Task.Run(() => context.Session.GetString("email"));
+    if (email == null)
+    {
+        return Results.NotFound("Session not found");
+    }
+    return Results.Ok(email);
+}
+
+async Task<IResult> ClearSession(HttpContext context)
+{
+    Console.WriteLine("ClearSession is called..Clearing session");
+    await Task.Run(context.Session.Clear);
+    return Results.Ok("Session cleared");
+}
+
+await app.RunAsync();
